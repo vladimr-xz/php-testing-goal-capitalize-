3159e5e20809eb974dcde9b37a528a0b6883880b
Vladimir <vladimir156xz@gmail.com>
2023-12-30 02:21:10 +0100

    added webmozart/assert

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vova_xz/test-learning',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1db574b51f69a0cfb5efcbf2b7213e5403ce2edb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vova_xz/test-learning' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1db574b51f69a0cfb5efcbf2b7213e5403ce2edb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webmozart/assert' => array(
+            'pretty_version' => '1.11.0',
+            'version' => '1.11.0.0',
+            'reference' => '11cb2199493b2f8a3b53e7f19068fc6aac760991',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webmozart/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
